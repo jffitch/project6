9c7d9b5262b02ac080047fc3d9e78efd2d242988
Jonathan Fitch <jfitch1@ymail.com>
2019-06-26 20:19:06 -0400

    Attempted fix of Worker code.

--- app/src/main/java/com/mathgeniusguide/project6/MainActivity.kt
+++ app/src/main/java/com/mathgeniusguide/project6/MainActivity.kt
@@ -14,7 +14,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val sdf = SimpleDateFormat("HH:mm:ss")
+        var sdf = SimpleDateFormat("yyyy/MM/dd")
+        val date = sdf.format(Date())
+        sdf = SimpleDateFormat("HH:mm:ss")
         val time = sdf.format(Date()).split(":")
         val SECONDS_IN_MINUTE = 60
         val SECONDS_IN_HOUR = 3600
@@ -22,16 +24,15 @@ class MainActivity : AppCompatActivity() {
         val delay =
             (SECONDS_IN_DAY + 10 * SECONDS_IN_MINUTE - time[0].toInt() * SECONDS_IN_HOUR - time[1].toInt() * SECONDS_IN_MINUTE - time[2].toInt()).toLong()
 
-            val constraints = Constraints.Builder()
-                .setRequiresBatteryNotLow(true)
-                .build()
-            val work = PeriodicWorkRequest.Builder(MoodWorker::class.java, 1, TimeUnit.DAYS)
-                .setConstraints(constraints)
-                .setInitialDelay(delay, TimeUnit.SECONDS)
-                .build()
-            WorkManager.getInstance(this)
-                .enqueueUniquePeriodicWork("dailyUpdate", if (time[0].toInt() < 6) ExistingPeriodicWorkPolicy.KEEP else ExistingPeriodicWorkPolicy.REPLACE, work)
-            Toast.makeText(this, "Time recorded as ${time}.\nDelay set to ${delay}.", Toast.LENGTH_LONG).show()
-
+        val constraints = Constraints.Builder()
+            .setRequiresBatteryNotLow(true)
+            .build()
+        val work = OneTimeWorkRequestBuilder<MoodWorker>()
+            .setConstraints(constraints)
+            .setInitialDelay(delay, TimeUnit.SECONDS)
+            .build()
+        WorkManager.getInstance(this)
+            .enqueueUniqueWork(date, ExistingWorkPolicy.KEEP, work)
+        Toast.makeText(this, "Time recorded as ${time}.\nDelay set to ${delay}.", Toast.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/com/mathgeniusguide/project6/work/MoodWorker.kt
+++ app/src/main/java/com/mathgeniusguide/project6/work/MoodWorker.kt
@@ -1,8 +1,7 @@
 package com.mathgeniusguide.project6.work
 
 import android.content.Context
-import androidx.work.Worker
-import androidx.work.WorkerParameters
+import androidx.work.*
 import com.mathgeniusguide.project6.R
 import com.mathgeniusguide.project6.dao.MoodsDao
 import com.mathgeniusguide.project6.database.AppDatabase
@@ -20,12 +19,30 @@ class MoodWorker(context: Context, workerParams: WorkerParameters) : Worker (con
         moodsDao = db?.moodsDao()
 
         var oneMood = db?.moodsDao()?.getOneMood()
-        val sdf = SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+        var sdf = SimpleDateFormat("yyyy/MM/dd")
         val date = sdf.format(Date())
 
         with(moodsDao) {
             this?.insertMoodIfNotExists(Moods(date, oneMood!!.emotion, applicationContext.getString(R.string.carried_over)))
         }
+
+        sdf = SimpleDateFormat("HH:mm:ss")
+        val time = sdf.format(Date()).split(":")
+        val SECONDS_IN_MINUTE = 60
+        val SECONDS_IN_HOUR = 3600
+        val SECONDS_IN_DAY = 86400
+        val delay =
+            (SECONDS_IN_DAY + 10 * SECONDS_IN_MINUTE - time[0].toInt() * SECONDS_IN_HOUR - time[1].toInt() * SECONDS_IN_MINUTE - time[2].toInt()).toLong()
+
+        val constraints = Constraints.Builder()
+            .setRequiresBatteryNotLow(true)
+            .build()
+        val work = OneTimeWorkRequestBuilder<MoodWorker>()
+            .setConstraints(constraints)
+            .setInitialDelay(delay, TimeUnit.SECONDS)
+            .build()
+        WorkManager.getInstance(applicationContext)
+            .enqueueUniqueWork(date, ExistingWorkPolicy.KEEP, work)
         return Result.success()
     }
 }
